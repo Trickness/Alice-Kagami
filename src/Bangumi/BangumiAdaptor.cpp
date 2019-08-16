82d2ff23066fc6c9b3216611331f07cddd2dd8ff
trickness <sternwzhang@outlook.com>
2019-08-16 21:12:21 +0800

    change to unix format

--- src/Bangumi/BangumiAdaptor.cpp
+++ src/Bangumi/BangumiAdaptor.cpp
@@ -1,4 +1,4 @@
-﻿#pragma execution_character_set("utf-8")
+#pragma execution_character_set("utf-8")
 
 #include "include/Bangumi/BangumiAdaptor.hpp"
 #include "src/third-party/gumbo-parser/Document.h"
